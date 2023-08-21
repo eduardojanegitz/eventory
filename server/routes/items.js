45f71dc89d60f7b79c138f64fc632bc7e23fe705
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2023-08-21 01:22:28 -0300

    delete server from this repo

--- server/routes/items.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getItems, getNewItems } from "../controllers/items.js";
-
-const router = express.Router();
-
-router.get("/items", getItems);
-router.get("/newitems", getNewItems);
-
-export default router;
