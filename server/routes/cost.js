079a3f4595e7bbc436bd2c811d280d9204c03796
Eduardo Alves <edu0alves00@gmail.com>
2023-08-30 12:28:38 -0300

    remove server folder

--- server/routes/cost.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { cost } from "../controllers/costController.js";
-
-const router = express.Router();
-
-router.get("/cost").get((req, res) => cost.getAllCost(req, res));
-router.get("/cost").post((req, res) => cost.postCost(req, res));
-
-export default router;
--- server/routes/tags.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {getTags } from "../controllers/tags.js";
-
-const router = express.Router();
-
-router.get("/tags", getTags);
-
-export default router;
