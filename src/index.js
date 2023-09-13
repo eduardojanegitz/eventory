3f75a48a71f69d75b9e4f6233f20413f9ec68e5a
Eduardo Alves <edu0alves00@gmail.com>
2023-09-13 04:28:24 -0300

    add provider to index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import { AuthProvider } from "context/AuthProvider";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
