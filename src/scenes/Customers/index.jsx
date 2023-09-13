c23684cd6028e1947e25fd374d36148d018b7033
Eduardo Alves <edu0alves00@gmail.com>
2023-09-13 13:15:39 -0300

    create edit and delete button in table

--- src/scenes/Customers/index.jsx
+++ src/scenes/Customers/index.jsx
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, useTheme } from "@mui/material";
-import { useGetCustomersQuery, useGetUserQuery } from "state/api";
+import { api2, useGetCustomersQuery, useGetUserQuery } from "state/api";
 import Header from "components/Header";
 import { DataGrid } from "@mui/x-data-grid";
 import FlexBetween from "components/FlexBetween";
-import PersonAddOutlinedIcon from '@mui/icons-material/PersonAddOutlined';
+import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
 import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { PopupExample } from "components/Popup";
 
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Customers = () => {
   const theme = useTheme();
   const { data, isLoading } = useGetUserQuery();
-  // console.log("data", data);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [department, setDeparment] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api2.post("api/user", {
+      username,
+      name,
+      email,
+      password,
+      department,
+    });
+  }
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const columns = [
+    {
+      field: "username",
+      headerName: "Nome de usuário",
+      flex: 1,
+    },
     {
       field: "name",
       headerName: "Nome",
@@ -30,6 +72,37 @@ const Customers = () => {
       headerName: "Departamento",
       flex: 1,
     },
+    {
+      field: "Ação",
+      flex: 1,
+      renderCell: (cellValues) => {
+        return (
+          <>
+            <Button
+              variant="contained"
+              color="secondary"
+              // onClick={(event) => {
+              //   handleClick(event, cellValues)
+              // }}
+              sx={{ marginRight: "5px" }}
+            >
+              Editar
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              startIcon={<DeleteIcon />}
+              // onClick={(event) => {
+              //   handleClick(event, cellValues)
+              // }}
+            >
+              Apagar
+            </Button>
+          </>
+        );
+      },
+    },
+
     // {
     //     field: "phoneNumber",
     //     headerName: "Número de telefone",
@@ -54,12 +127,18 @@ const Customers = () => {
     //   flex: 0.5,
     // },
   ];
+
+  const showToastMessage = () => {
+    toast.success("Usuário cadastrado com sucesso!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
-      <Header title="USUÁRIOS" subtitle="Lista de usuários" />
-      <Box>
-          <Button
+        <Header title="USUÁRIOS" subtitle="Lista de usuários" />
+        <Box>
+          {/* <Button
             sx={{
               backgroundColor: theme.palette.secondary.light,
               color: theme.palette.background.alt,
@@ -72,12 +151,82 @@ const Customers = () => {
               <PersonAddOutlinedIcon sx={{ mr: "10px" }} />
               Novo usuário
             </Link>
+          </Button> */}
+
+          {// CRIAR UM COMPONENTE PARA ISSO
+          }
+          <Button
+            onClick={handleOpen}
+            sx={{
+              backgroundColor: theme.palette.secondary.dark,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            NOVO USUÁRIO
           </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Cadastro de novo usuário
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <form onSubmit={handleSubmit} className="form">
+                  <input
+                    type="text"
+                    placeholder="Usuário"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="input"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="input"
+                  />
+                  <input
+                    type="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="input"
+                  />
+                  <input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="input"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Departamento"
+                    value={department}
+                    onChange={(e) => setDeparment(e.target.value)}
+                    className="input"
+                  />
+                  <button onClick={showToastMessage} className="btn-submit">
+                    Cadastrar
+                  </button>
+                  {/* <PopupExample />  */}
+                </form>
+              </Typography>
+            </Box>
+          </Modal>
         </Box>
       </FlexBetween>
       <Box
-        mt="40px"
-        height="90vh"
+        mt="20px"
+        height="80vh"
         sx={{
           "& .MuiDataGrid-root": {
             border: "none",
