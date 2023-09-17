a968159f927bc78098b911c0a1c028e87b228978
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2023-09-17 15:39:13 -0300

    improving asset movement

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,26 @@
+import { InputBase, useTheme } from "@mui/material";
 import React from "react";
 
-const Input = ({user}) => {
- return (
-    <div>
-        <input type="text" value={user.name}/>
-    </div>
- )
-}
+const Input = ({ type, placeholder, value, onChange, onBlur, readOnly }) => {
+  const theme = useTheme();
+  return (
+    <InputBase
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+      disabled={readOnly}
+      sx={{
+        border: ` 2px solid ${theme.palette.primary[100]}`,
+        borderRadius: "20px",
+        padding: "7px",
+        boxShadow: `4.5px 2.5px ${theme.palette.primary[100]}`,
+        width: "30rem",
+        marginBottom: "15px"
+      }}
+    />
+  );
+};
 
 export default Input;
