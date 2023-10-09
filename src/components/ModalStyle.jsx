6753955746c9e05238cedd20ae1b69303e3914fc
Eduardo Alves <edu0alves00@gmail.com>
2023-10-09 17:16:30 -0300

    refactoring modal component

--- src/components/ModalStyle.jsx
+++ src/components/ModalStyle.jsx
@@ -1,27 +1,32 @@
 import { Box, Modal, Typography, useTheme } from "@mui/material";
 import React from "react";
-import Input from "./Input";
 
-const ModalStyle = ({children}) => {
-    const theme = useTheme();
+const ModalStyle = ({ children, maxHeight, open, onClose }) => {
+  const theme = useTheme();
 
   const style = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "37.5rem",
+    maxWidth: "100%",
+    width: "auto",
+    maxHeight: maxHeight || "none",
+    overflowY: maxHeight ? "auto" : "visible",
     bgcolor: theme.palette.background.default,
     borderRadius: "35px",
     boxShadow: 24,
     padding: "2rem",
   };
   return (
-    
-    <Box sx={style}>
-     {children}
-    </Box>
-
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>{children}</Box>
+    </Modal>
   );
 };
 
