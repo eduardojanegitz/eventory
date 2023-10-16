ce0f3d41475bda846e8d6988c1cf33bed6c0171a
Eduardo Alves <edu0alves00@gmail.com>
2023-10-16 20:13:31 -0300

    putting an InputLabel

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,30 +1,26 @@
-import { useTheme } from "@emotion/react";
-import { InputLabel, Select } from "@mui/material";
 import React from "react";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 const Dropdown = ({ children, value, onChange, label, id }) => {
   const theme = useTheme();
+
   return (
-    <Select
-    sx={{
-      width: "100%",
-    }}
-      // sx={{
-      //   border: ` 2px solid ${theme.palette.primary[100]}`,
-      //   borderRadius: "20px",
-      //   padding: "7px",
-      //   width: "100%",
-      //   boxShadow: `4.5px 2.5px ${theme.palette.primary[100]}`,
-      //   height: "2.9rem",
-      // }}
-      value={value}
-      onChange={onChange}
-      id={id}
-      label={label}
-      color="secondary"
-    >
-      {children}
-    </Select>
+    <FormControl fullWidth>
+      <InputLabel color="secondary" id={id}>
+        {label}
+      </InputLabel>
+      <Select
+        required
+        label={label}
+        value={value}
+        onChange={onChange}
+        id={id}
+        color="secondary"
+      >
+        {children}
+      </Select>
+    </FormControl>
   );
 };
 
